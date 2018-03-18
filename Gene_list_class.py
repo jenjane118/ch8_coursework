e4b52c1d2cb3fe7a7bb2112aed987bcb87f3c40d
jjstiens <37145031+jjstiens@users.noreply.github.com>
2018-03-18 16:47:21 +0000

    Update Gene_list_class.py

--- Gene_list_class.py
+++ Gene_list_class.py
@@ -6,7 +6,7 @@
 Program:    Gene_list
 File:       Gene_list.py
 
-Version:    dummy
+Version:    1.0
 Date:       15.03.18
 Function:   Retrieve all gene identifier info for every gene on
             chromosome 8 and returns as a list
@@ -31,19 +31,20 @@ Usage:
 Revision History:
 =================
 
-
+18.03.18    Revised to incorporate methods      JJS
 
 """
 
 #******************************************************************************
 # Import libraries
 
-
+import os
 
 #*****************************************************************************
 
 
-xmlTemplate = """<gene>
+xmlTemplate = """
+<gene>
 <geneidentifiers>
     <accession>%(acc)s</accession>
     <genid>%(genid)s</genid>
@@ -61,16 +62,57 @@ identity =[('AB061209', 'MRPS28', 'mitochondrial ribosomal protein s28', '8q21.1
 
 
 class Gene:
-    def __init__(self, acc= str(), genid=str(), product=str(), location=str()):
+    """ A class for handling associated gene identifiers and sequence data."""
+    count = 0
+    names = []
+
+    @staticmethod
+    def total():
+        """ Static method to return current number of gene objects in file.
+        """
+        return Gene.count
+    
+    def __init__(self, acc= str() , genid = str(), product=str(), location=str()):
+        self.number     = Gene.count
         self.acc        = acc
         self.genid      = genid
         self.product    = product
         self.location   = location
+        Gene.count      += 1
 
+    def __str__(self):
+        rep = ''
+        rep += 'acc: ' + self.acc + '\n' + 'genid: ' + self.genid + '\n' + 'product: ' \
+               + self.product  + '\n' + 'location: ' + self.location
+        return rep
+ 
+    def Gene_dict(self):
+        """Create a dictionary for mapping to xml template.
+        """
+        
+        gene_dict = {'acc': self.acc, 'genid': self.genid, 'product': self.product, 'location': self.location} 
+        return gene_dict
+
+
+
+## Writes output to file in xml format
 with open('genelist_output.txt', 'w') as text_file:
+    print('<?xml version="1.0" encoding="UTF-8"?>\n', file=text_file)
+    
+    gene_list = []
+    gene_map = {}
+
+## Uses initialisation functin to create a gene object for each listing from database
+## Had to put into a list as function won't work straight from tuple    
     for x in identity:
-        gene = Gene(x)
-        gene_map = {'acc': x[0], 'genid': x[1], 'product': x[2], 'location': x[3]}
+        gene_list.append(Gene(x[0], x[1], x[2], x[3]))
+        
+## Uses gene dictionary function to create a mapping of attributes for printing in xml
+    for y in gene_list:
+        gene_map = y.Gene_dict()
         print(xmlTemplate%gene_map, file=text_file)
+
+## Prints total number of gene objects using static function
+print(Gene.total())
 
 
