9fc4c1610771065c6eb3bd8bd01a2de099765af0
jenjane118 <37145031+jenjane118@users.noreply.github.com>
2018-03-21 16:48:46 +0000

    Add files via upload
    This file will search for one of three pre-loaded restriction enzymes or a custom sequence. Restriction sites are indicated in returned sequence surrounded by '@' symbol.

--- enzyme_dummy.py
+++ enzyme_dummy.py
@@ -4,22 +4,41 @@
 ## dummy file for testing out restriction enzyme cutting sites
 
 import re
-from re import finditer
+import sys
 
 
 
-def get_enzyme(enzyme, sequence):
-    """Get restriction enzyme cutting sequence
-        return cutting sites (base numbers) in dna sequence."""
-    cut_list = []
-    for match in finditer(enzyme, sequence):
-        cut_list.append(match.span())
-    return cut_list
-
+def show_cutDNA(enzyme, sequence):
+    """ Get restriction enzyme cutting sequence and return dna seq with indicated
+        cutting sites (insert @ symbol)."""
+    
+    count = 0
+    cut = ''
+    if enzyme == 'EcoRI':
+        cut = 'GAATTC'
+    elif enzyme == 'BamHI':
+        cut = 'GG.CC'
+    elif enzyme == 'BsuMI':
+        cut = 'CTCGAG'
+    else:
+        cut = enzyme
+            
+    new_seq = sequence.replace(' ', '')
+    cut_seq = ''
+    p = re.compile(r'('+cut+')')
+    it = p.finditer(new_seq)
+    for match in it:
+        count += 1
+    cut_seq = re.sub(r'('+cut+')', r'@\1@', new_seq)
+    print(count)
+    return cut_seq
     
 
-enz = 'fake enzyme'
-fake_cut = 'AATGCAGT'
+
+## You can use either enzyme name ('EcoRI', 'BamHI', or 'BsuMI') or enter custom
+## restriction sequence
+
+enz = 'GTGGCG'
 
 acc  = 'AB12345'
 seq1 = 'ATGGCAATGCAGTGGCGCTGTGTCGGACCCGTGCTGTGGCTGCCGAGAGCCATTTTCTGCGAGTGTTTCTCTTCTTCA\
@@ -32,30 +51,13 @@ seq1 = 'ATGGCAATGCAGTGGCGCTGTGTCGGACCCGTGCTGTGGCTGCCGAGAGCCATTTTCTGCGAGTGTTTCTCT
         GAGGCTAATGCAGTTCTCTTGGGAATCCAGGAGAGTAAAGACTCAAGATCGAAAGAAGAACATCATGAAA\
         AAT'
 
-##  run get_enzyme function and output into list       
+## call function with paramenters of enzyme and sequence
 
-span_list = []
-span_list = get_enzyme(fake_cut, seq1)
+new_seq = show_cutDNA(enz, seq1)
 
-## convert list of match items to strings. Add one to each position (starts at 0). Append to new list
-new_list =[]
-n_cuts = 0
-#print(span_list)
-for x in span_list:
-    n_cuts += 1
-    span   =  str(x[0]+1) + '-' + str(x[1]+1)
-    new_list.append(span)
-#print(new_list)
-#print(n_cuts)
-
-
-## print output in xml to file
+## write output in xml to file
 
 from xml.dom import minidom
-import sys
-
-dna = seq1.replace(' ', '')
-
 
 doc = minidom.Document()
 
@@ -64,32 +66,23 @@ gene.setAttribute('acc', acc)
 doc.appendChild(gene)
 
 sequence = doc.createElement('sequence')
-text = doc.createTextNode(dna)
-sequence.appendChild(text)
 gene.appendChild(sequence)
 
-for i in range (0, n_cuts):
-    enzyme = doc.createElement('enzyme')
-    enzyme.setAttribute('name', enz)
+enzyme = doc.createElement('enzyme')
+enzyme.setAttribute('name', enz)
 
-    sequence.appendChild(enzyme)
+sequence.appendChild(enzyme)
 
-    cut_site = doc.createElement('span')
-    text = doc.createTextNode(new_list[i])
-    cut_site.appendChild(text)
-    enzyme.appendChild(cut_site)
+cut_site = doc.createElement('cut_seq')
+text = doc.createTextNode(new_seq)
+cut_site.appendChild(text)
+enzyme.appendChild(cut_site)
 
 doc.writexml(sys.stdout, addindent='    ', newl='\n')
     
-file_handle = open('enz_dummy.xml', 'w')
+file_handle = open('enz_dummy_out.xml', 'w')
 doc.writexml(file_handle, addindent='   ',newl='\n')
 file_handle.close()
 
 
 
-
-
-
-
-
-    
