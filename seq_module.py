56b28e95993652428e97fb175af2d2121384963c
Jennifer J. Stiens <j.j.stiens@gmail.com>
2018-04-18 15:42:10 +0100

    worked out as module

--- seq_module.py
+++ seq_module.py
@@ -28,6 +28,8 @@ Usage:
 
 Revision History:
 =================
+V1.0            Original                        By: JJS
+V1.1            Revised as module                   JJS
 
 """
 #*****************************************************************************
@@ -38,7 +40,6 @@ import re
 
 
 #****************************************************************************
-
 
 
 def getSequence(acc, seq):
@@ -98,61 +99,72 @@ def codingSeq(acc, seq, exon_list=None):
             coding_seq += seq[start:end]
             #coding_seq = coding_seq[code_start:]
         return coding_seq
+    else:
+        return coding_seq
 
-# dummy data (sequence in file, acc and exon_list below)
-with open ('seq_file1.txt', 'r') as f:
-    file = f.read().splitlines()
 
-exon_list = [('AB12345.1', 36, 807), ('AB232010.1', 2222, 2311)]
-acc = 'AB12345.1'
+
+# main
+if __name__ == "__main__":
+
+## dummy data (sequence in file, acc and exon_list below)
+    with open ('seq_file1.txt', 'r') as f:
+        file = f.read().splitlines()
+    f.close()
+
+    exon_list = [('AB12345.1', 36, 807), ('AB232010.1', 2222, 2311)]
+    acc = 'AB12345.1'
 
 # call functions
-line_seq = getSequence(acc, file)
+    line_seq = getSequence(acc, file)
 
-ann_seq = annotateSeq(acc, file, exon_list)
+    ann_seq = annotateSeq(acc, file, exon_list)
 
 #print(ann_seq)
 
-code_seq = codingSeq(acc, file, exon_list)
+    code_seq = codingSeq(acc, file, exon_list)
 
 #print(code_seq)
 
 
 # print to file in xml format
-xml_seq_ann = """
-<gene>
-<sequence acc=%(acc)s>
-<ann_seq>%(sequence)s</ann_seq>
-</sequence>
-</gene>
-"""
-seq_map = {'acc': acc, 'sequence': ann_seq}
-
-xml_seq_coding = """
-<gene>
-<sequence acc=%(acc)s>
-<coding_seq>%(coding_seq)s</coding_seq>
-</sequence>
-</gene>
-"""
-seq_map2 = {'acc':acc, 'coding_seq': code_seq}
+    xml_seq_ann = """
+    <gene>
+    <sequence acc=%(acc)s>
+    <ann_seq>%(sequence)s</ann_seq>
+    </sequence>
+    </gene>
+    """
 
 
+    seq_map = {'acc': acc, 'sequence': ann_seq}
 
-# # Print out the annotated gene sequence of a particular gene in xml format:
-# with open('sequence_output.txt', 'w') as text_file:
-#     # I can't manage to get numbered sequence to show up in xml--it is in form of dictionary (line_seq).
-#     # here is script for printing dictionary in tidy rows with line numbers:
-#     for i in range(1, len(line_seq), 60):
-#         print(i, '    ', end='', file=text_file)
-#         for j in range(i, i + 59):
-#             if j in line_seq:
-#                 print(line_seq[j], end='', file=text_file)
-#             else:
-#                 pass
-#         print('\n', file=text_file)
-#     print('<?xml version="1.0" encoding="UTF-8"?>', file=text_file)
-#     print(xml_seq_ann %seq_map, file=text_file)
-#     print(xml_seq_coding %seq_map2, file=text_file)
-#
-#
+    xml_seq_coding = """
+    <gene>
+    <sequence acc=%(acc)s>
+    <coding_seq>%(coding_seq)s</coding_seq>
+    </sequence>
+    </gene>
+    """
+    seq_map2 = {'acc':acc, 'coding_seq': code_seq}
+
+
+
+## # Print out the annotated gene sequence of a particular gene in xml format:
+    with open('sequence_output.txt', 'w') as text_file:
+    # I can't manage to get numbered sequence to show up in xml--it is in form of dictionary (line_seq).
+    # here is script for printing dictionary in tidy rows with line numbers:
+        for i in range(1, len(line_seq), 60):
+            print(i, '    ', end='', file=text_file)
+            for j in range(i, i + 59):
+                if j in line_seq:
+                    print(line_seq[j], end='', file=text_file)
+                else:
+                    pass
+            print('\n', file=text_file)
+        print('<?xml version="1.0" encoding="UTF-8"?>', file=text_file)
+        print(xml_seq_ann %seq_map, file=text_file)
+        print(xml_seq_coding %seq_map2, file=text_file)
+    text_file.close()
+
+
